497546c56fa3227c22c5bfd973796a2f84907967
Gerwin Brunner <gerwin.brunner@gmail.com>
2016-08-22 09:18:00 +0200

    test with remote wsdl

---
+++ server/main.js
@@ -0,0 +1,31 @@
+import { Meteor } from 'meteor/meteor';
+
+
+Meteor.startup(() => {
+  // code to run on server at startup
+
+  let url = Meteor.absoluteUrl()+'BLZService.xml';
+  //let url = 'http://www.thomas-bayer.com/axis2/services/BLZService?wsdl';
+
+  let args = {};
+  console.log("server started", url);
+
+  try {
+    var client = Soap.createClient(url);
+    console.log("client", client);
+
+    var result = client.getBank(args);
+
+    console.log(result);
+  }
+  catch (err) {
+    if(err.error === 'soap-creation') {
+      console.log('SOAP Client creation failed', err);
+    }
+    else if (err.error === 'soap-method') {
+      console.log('SOAP Method call failed', err);
+    }
+  }
+
+  console.log("SOAP call done", url);
+});
